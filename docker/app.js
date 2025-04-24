7258810d97d8c1cdb25d4d8ee6b3aa1aae3b62ff
Samvel Khachatryan <samvelkhachatryan@Mac-mini-Samvel.local>
2025-04-24 13:59:23 +0400

    Updated app code

--- docker/app.js
+++ docker/app.js
@@ -1,12 +1,12 @@
-// app.js
 const express = require('express');  
 const app = express();               
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello, Samvel');
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running at http://0.0.0.0:${PORT}`);
 });
+
